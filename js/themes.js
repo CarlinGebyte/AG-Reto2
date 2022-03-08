2e510a2636e3bc0c922622e1334a44f13b96785a
CarlinGebyte <91474067+CarlinGebyte@users.noreply.github.com>
2022-03-07 21:37:22 -0500

    adding prefers-color-scheme and localStorage

--- js/themes.js
+++ js/themes.js
@@ -1,19 +1,56 @@
 let themeSelectors = document.querySelectorAll(".toggle");
 let themeStyle = document.getElementById("choose-theme");
+let localTheme = "1";
 
-
-themeSelectors.forEach(theme => {
-    theme.addEventListener("click", (e) => {
-        e.preventDefault();
-        selectTheme(theme.value);
-    });
+themeSelectors.forEach((theme) => {
+  theme.addEventListener("click", (e) => {
+    e.preventDefault();
+    selectTheme(theme.value);
+  });
 });
 
-
 const selectTheme = (theme) => {
-    if (theme === "1"){
-        themeStyle.setAttribute('href', '');
-    } else {
-        themeStyle.setAttribute('href', `css/theme-${theme}.css`)
+  if (theme === "1") {
+    themeStyle.setAttribute("href", "");
+    localStorage.setItem("theme", "1");
+  } else {
+    themeStyle.setAttribute("href", `css/theme-${theme}.css`);
+    localStorage.setItem("theme", `${theme}`);
+  }
+};
+
+const setLocalTheme = () => {
+  localTheme = localStorage.getItem("theme");
+  if (localTheme !== undefined) {
+    switch (localTheme) {
+      case "1":
+        themeStyle.setAttribute("href", "");
+        break;
+      case "2":
+      case "3":
+        themeStyle.setAttribute("href", `css/theme-${localTheme}.css`);
+        break;
+      default:
+        break;
     }
-}
+  }
+};
+
+const windowsTheme = () => {
+  const lighT = window.matchMedia("(prefers-color-scheme: light)").matches;
+  const darkT = window.matchMedia("(prefers-color-scheme: dark)").matches;
+  console.log(lighT, darkT);
+
+  if (lighT) {
+    themeStyle.setAttribute("href", "css/theme-2.css");
+  } else if (darkT) {
+    themeStyle.setAttribute("href", "css/theme-3.css");
+  } else {
+    themeStyle.setAttribute("href", "");
+
+  }
+};
+
+windowsTheme();
+
+setLocalTheme();
